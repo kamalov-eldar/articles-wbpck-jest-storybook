ec3bd927d8a1ad4bd70ce36aba123896a865d3f9
kamalov-eldar <kamalov-eldar@mail.ru>
2023-07-24 20:49:47 +0300

    style sidebar

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,9 +1,9 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import React from 'react';
-import { Theme, useTheme } from 'app/providers/ThemeProvider';
-import LightIcon from 'shared/assets/icons/theme-light.svg';
-import DarkIcon from 'shared/assets/icons/theme-dark.svg';
-import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { classNames } from "shared/lib/classNames/classNames";
+import React from "react";
+import { Theme, useTheme } from "app/providers/ThemeProvider";
+import LightIcon from "shared/assets/icons/theme-light.svg";
+import DarkIcon from "shared/assets/icons/theme-dark.svg";
+import { Button, ButtonTheme } from "shared/ui/Button/Button";
 
 interface ThemeSwitcherProps {
     className?: string;
@@ -13,11 +13,7 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
     const { theme, toggleTheme } = useTheme();
 
     return (
-        <Button
-            theme={ThemeButton.CLEAR}
-            className={classNames('', {}, [className])}
-            onClick={toggleTheme}
-        >
+        <Button theme={ButtonTheme.CLEAR} className={classNames("", {}, [className])} onClick={toggleTheme}>
             {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
         </Button>
     );
