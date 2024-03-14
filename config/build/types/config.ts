2d9abb0a27e48559f061cab4eccc5ddac7fcaec6
kamalov-eldar <kamalov-eldar@mail.ru>
2024-03-14 18:21:22 +0300

    deploy vercel

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -1,10 +1,12 @@
-export type BuildMode = 'production' | 'development';
+export type BuildMode = "production" | "development";
 
 export interface BuildPaths {
     entry: string;
     build: string;
     html: string;
     src: string;
+    locales: string;
+    buildLocales: string;
 }
 
 export interface BuildEnv {
@@ -19,5 +21,5 @@ export interface BuildOptions {
     isDev: boolean;
     port: number;
     apiUrl: string;
-    project: 'storybook' | 'frontend' | 'jest';
+    project: "storybook" | "frontend" | "jest";
 }
