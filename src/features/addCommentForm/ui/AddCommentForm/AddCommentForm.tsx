54e7d8374296ff096ff38e651134c3636859c038
kamalov-eldar <kamalov-eldar@mail.ru>
2024-03-17 19:55:00 +0300

    90

--- src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.tsx
+++ src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.tsx
@@ -5,15 +5,9 @@ import { classNames } from '@/shared/lib/classNames/classNames';
 import { Input } from '@/shared/ui/Input/Input';
 import { Button, ButtonTheme } from '@/shared/ui/Button/Button';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
-import {
-    DynamicModuleLoader,
-    ReducersList,
-} from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { DynamicModuleLoader, ReducersList } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { HStack } from '@/shared/ui/Stack';
-import {
-    addCommentFormActions,
-    addCommentFormReducer,
-} from '../../model/slices/addCommentFormSlice';
+import { addCommentFormActions, addCommentFormReducer } from '../../model/slices/addCommentFormSlice';
 import {
     getAddCommentFormError,
     getAddCommentFormText,
@@ -36,12 +30,9 @@ const AddCommentForm = memo((props: AddCommentFormProps) => {
     const error = useSelector(getAddCommentFormError);
     const dispatch = useAppDispatch();
 
-    const onCommentTextChange = useCallback(
-        (value: string) => {
-            dispatch(addCommentFormActions.setText(value));
-        },
-        [dispatch],
-    );
+    const onCommentTextChange = useCallback((value: string) => {
+        dispatch(addCommentFormActions.setText(value));
+    }, [dispatch]);
 
     const onSendHandler = useCallback(() => {
         onSendComment(text || '');
@@ -50,18 +41,17 @@ const AddCommentForm = memo((props: AddCommentFormProps) => {
 
     return (
         <DynamicModuleLoader reducers={reducers}>
-            <HStack
-                justify="between"
-                max
-                className={classNames(cls.AddCommentForm, {}, [className])}
-            >
+            <HStack justify="between" max className={classNames(cls.AddCommentForm, {}, [className])}>
                 <Input
                     className={cls.input}
                     placeholder={t('Введите текст комментария')}
                     value={text}
                     onChange={onCommentTextChange}
                 />
-                <Button theme={ButtonTheme.OUTLINE} onClick={onSendHandler}>
+                <Button
+                    theme={ButtonTheme.OUTLINE}
+                    onClick={onSendHandler}
+                >
                     {t('Отправить')}
                 </Button>
             </HStack>
