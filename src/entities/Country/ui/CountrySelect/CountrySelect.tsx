b6aeb7a46ef9822ea3ec67a2e2894c45d4aed25b
kamalov-eldar <kamalov-eldar@mail.ru>
2024-03-18 17:15:28 +0300

    96

--- src/entities/Country/ui/CountrySelect/CountrySelect.tsx
+++ src/entities/Country/ui/CountrySelect/CountrySelect.tsx
@@ -3,6 +3,11 @@ import { memo, useCallback } from 'react';
 import { ListBox } from '@/shared/ui/Popups';
 import { Country } from '../../model/types/country';
 
+import { Counter } from '@/entities/Counter';
+
+{
+    Counter;
+}
 interface CountrySelectProps {
     className?: string;
     value?: Country;
@@ -18,24 +23,27 @@ const options = [
     { value: Country.Ukraine, content: Country.Ukraine },
 ];
 
-export const CountrySelect = memo(({
-    className, value, onChange, readonly,
-}: CountrySelectProps) => {
-    const { t } = useTranslation();
+export const CountrySelect = memo(
+    ({ className, value, onChange, readonly }: CountrySelectProps) => {
+        const { t } = useTranslation();
 
-    const onChangeHandler = useCallback((value: string) => {
-        onChange?.(value as Country);
-    }, [onChange]);
+        const onChangeHandler = useCallback(
+            (value: string) => {
+                onChange?.(value as Country);
+            },
+            [onChange],
+        );
 
-    return (
-        <ListBox
-            onChange={onChangeHandler}
-            value={value}
-            defaultValue={t('Укажите страну')}
-            label={t('Укажите страну')}
-            items={options}
-            readonly={readonly}
-            direction="top right"
-        />
-    );
-});
+        return (
+            <ListBox
+                onChange={onChangeHandler}
+                value={value}
+                defaultValue={t('Укажите страну')}
+                label={t('Укажите страну')}
+                items={options}
+                readonly={readonly}
+                direction="top right"
+            />
+        );
+    },
+);
